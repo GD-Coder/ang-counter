af77f8f625848c9d4348aa0d956a9ebd7d138e1f
Gerald Downey <gerald.downey@scale.tech>
2025-07-19 12:38:49 -0700

    Implemented start and stop functionality

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
-import { Component, signal, effect } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { Component, signal, effect } from "@angular/core";
 
 // Import child components here
 import { SelectBoxComponent } from "./components/selectbox/selectbox";
@@ -32,6 +32,7 @@ import { StatusBannerComponent } from "./components/status-banner/status-banner"
 })
 export class App {
   protected readonly title = signal("MD Counter");
+  private pollingIntervalId: number = 0;
   machines = signal([
     "Machine 1",
     "Machine 2",
@@ -48,27 +49,26 @@ export class App {
     "Machine 14",
     "Machine 15",
   ]);
+  sunVisible = signal(false);
   operators = signal(["Johnny Walker", "Jim Beam", "Evan Williams"]);
 
-  selectedMachine = signal(this.machines()[0]);
-  selectedOperator = signal(this.operators()[0]);
-
-  targetRate = signal(525);
-  averageRate = signal(0);
   totalCount = signal(0);
-  currentCount = signal(0);
   percentage = signal(0);
+  averageRate = signal(0);
+  currentCount = signal(0);
+  targetRate = signal(525);
   totalDowntime = signal(0);
   currentDowntime = signal(0);
-  avgThreshold = signal({ low: 225, mid: 340 });
-  countThreshold = signal({ low: 15000, mid: 34000 });
+  selectedMachine = signal(this.machines()[0]);
   percThreshold = signal({ low: 50, mid: 75 });
+  avgThreshold = signal({ low: 225, mid: 340 });
+  selectedOperator = signal(this.operators()[0]);
+  countThreshold = signal({ low: 15000, mid: 34000 });
 
+  // Select machine
   showControls = signal(
     localStorage.getItem("showControls") === "false" ? false : true
   );
-
-  sunVisible = signal(false);
 
   constructor() {
     // Set sun visibility after delay
@@ -78,70 +78,65 @@ export class App {
     effect(() => {
       localStorage.setItem("showControls", this.showControls().toString());
     });
-
-    // Polling logic
-    this.startPolling();
   }
-
+  // Start button
   handleStart() {
-    console.log("Start Clicked...");
-    // Add actual logic here
+    if (this.pollingIntervalId === 0) {
+      this.startPolling();
+    } else {
+      console.log("Polling already in progress.");
+    }
   }
-
+  // Stop button
   handleStop() {
-    console.log("Stop Clicked...");
-    // Add actual logic here
+    if (this.pollingIntervalId !== 0) {
+      console.log(this.pollingIntervalId);
+      clearInterval(this.pollingIntervalId);
+      this.pollingIntervalId = 0;
+    }
   }
-
+  // Start polling
   async startPolling() {
-    const fetchCounts = async () => {
-      try {
-        const [average, current, total, totalDT, currentDT] = await Promise.all(
-          [
-            fetch("http://127.0.0.1:3200/average-count").then((res) =>
-              res.json()
-            ),
-            fetch("http://127.0.0.1:3200/current-count").then((res) =>
-              res.json()
-            ),
-            fetch("http://127.0.0.1:3200/total-count").then((res) =>
-              res.json()
-            ),
-            fetch("http://127.0.0.1:3200/total-downtime").then((res) =>
-              res.json()
-            ),
-            fetch("http://127.0.0.1:3200/current-downtime").then((res) =>
-              res.json()
-            ),
-          ]
-        );
-
-        this.averageRate.set(average.count);
-        this.currentCount.set(current.count);
-        this.totalCount.set(total.count);
-        this.totalDowntime.set(totalDT.count);
-        this.currentDowntime.set(currentDT.count);
-
-        const percent = (average.count * 100) / this.targetRate();
-        this.percentage.set(percent);
-      } catch (err) {
-        console.error("Polling error:", err);
-      }
-    };
-
-    await fetchCounts();
-    setInterval(fetchCounts, 5000);
+    await this.fetchCounts();
+    this.pollingIntervalId = setInterval(() => {
+      this.fetchCounts();
+    }, 5000);
   }
-
+  // Show/Hide controls
   toggleControls() {
     this.showControls.set(!this.showControls());
   }
-
+  // Select machine
   selectMachine(machine: string) {
     this.selectedMachine.set(machine);
   }
-
+  // Select operator
   selectOperator(operator: string) {
     this.selectedOperator.set(operator);
   }
+  // Get all counts
+  fetchCounts = async () => {
+    try {
+      const [average, current, total, totalDT, currentDT] = await Promise.all([
+        fetch("http://127.0.0.1:3200/average-count").then((res) => res.json()),
+        fetch("http://127.0.0.1:3200/current-count").then((res) => res.json()),
+        fetch("http://127.0.0.1:3200/total-count").then((res) => res.json()),
+        fetch("http://127.0.0.1:3200/total-downtime").then((res) => res.json()),
+        fetch("http://127.0.0.1:3200/current-downtime").then((res) =>
+          res.json()
+        ),
+      ]);
+
+      this.averageRate.set(average.count);
+      this.currentCount.set(current.count);
+      this.totalCount.set(total.count);
+      this.totalDowntime.set(totalDT.count);
+      this.currentDowntime.set(currentDT.count);
+
+      const percent = (average.count * 100) / this.targetRate();
+      this.percentage.set(percent);
+    } catch (err) {
+      console.error("Polling error:", err);
+    }
+  };
 }
